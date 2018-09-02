09a118bb4b1be776a9974cd50ac44e8780699570
Pavel <saswwwx@gmail.com>
2018-09-02 15:39:52 +0300

    Add json parser

--- src/main/scala/movies_rank/MoviesRank.scala
+++ src/main/scala/movies_rank/MoviesRank.scala
@@ -11,6 +11,15 @@ object MoviesRank {
       .appName("Movies rank")
       .getOrCreate()
 
+    /* Parse json(with type: Array[Map[String -> String]]) to List of some key's values.
+    	Example:
+      jsonToList("key1")([{"key1": "first1", "key2": "second1"}, {"key1": "first2", "key2": "second2"}]) =
+    	List("first1", "first2") */
+    def jsonToList(key: String)(inputString: String): List[String] = {
+      val map = new Gson().fromJson(inputString, classOf[java.util.List[java.util.Map[String, String]]])
+      return map.toList.map(x => x(key).trim)
+    }
+
     val movies_df_raw = spark.read.format("csv")
 	    .option("header", "true")
 	    .option("multiLine", true)
